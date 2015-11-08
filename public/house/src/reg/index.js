55cc1ec8f6d4f9e58f486f64b6282797c661d4da
暁麦 <panpan.hpp@alibaba-inc.com>
2015-11-08 19:04:21 +0800

    reh

--- public/house/src/reg/index.js
+++ public/house/src/reg/index.js
@@ -1,10 +1,12 @@
 $(function(){
+	var domain='http://121.40.70.168';
 	var App={
 		init:function(){
 
 			this.bindEvents();
 		},
 		bindEvents:function(){
+			var self=this;
 			$('.check-field').on('click',function(){
 
 				if($(this).hasClass('checked-field')){
@@ -13,6 +15,10 @@ $(function(){
 					$(this).addClass('checked-field');
 				}
 			})
+			$('.J_Shop').on('keyup',function(){
+				var name=$(this).val();
+				self.renderSuggest(name);
+			});
 
 			$('.J_Reg').on('click',function(){
 				$('.J_Mask').show();
@@ -26,9 +32,28 @@ $(function(){
 
 			})
 
-			$('.J_Submit').on('click',function(){
+			$('.J_Suggest').delegate('.item','click',function(){
+				var id=$(this).attr('cid');
+				$('#company_id').val(id);
+				$('.J_Shop').val($(this).text());
+				$('.J_Suggest').hide();
 
+			})
 
+		},
+		renderSuggest:function(name){
+			$.get(domain+'/api/base/search_company',{name:name},function(data){
+				var dom=$('.J_Suggest');
+				var html=[];
+				$.each(data.data.list,function(i,t){
+					html.push('<div class="item" cid="'+t.id+'">'+t.name+'</div>');
+				})
+				dom.html(html.join(''));
+				if(data.data.list.length>0){
+					dom.show();
+				}else{
+					dom.hide();
+				}
 			})
 
 		}
